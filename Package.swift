86d8921ccf7757c87e024a9d9f32406de095a08f
Tiago Santo <tiago@test.com>
2024-10-11 15:05:18 +0100

    First commit

--- Package.swift
+++ Package.swift
@@ -2,5 +2,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyCam"
+    name: "SwiftyCam",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "SwiftyCam",
+            targets: ["SwiftyCam"]
+        )
+    ],
+    targets: [
+        .target(name: "SwiftyCam"),
+    ]
 )
